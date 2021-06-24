292832c3f2b7d28f5f64f734bb5bc912d1c4f609
dv <diabeticvader@protonmail.com>
2021-06-24 08:47:19 -0400

    updated teh function organiztion plus some TODOs

--- sheet-exp.py
+++ sheet-exp.py
@@ -6,38 +6,18 @@ from apiclient import discovery #need
 from google.oauth2 import service_account # need
 import gspread # need
 import os.path # need
-#from googleapiclient.discovery import build
-#from google_auth_oauthlib.flow import InstalledAppFlow
-#from google.auth.transport.requests import Request
-#from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from apiclient.http import MediaFileUpload # need
 import pickle # need
 import datetime # need
 import pandas as pd #need
-import numpy as np #nned
-
-
-def populate_spreadsheet():
-    #passing in created spreadsheet id from create_spreadsheet function
-    #Scopes that we are using for the APIs TODO: make an auth function
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
-              'https://www.googleapis.com/auth/drive']
-    #Credentials
-    secret_file = os.path.join(os.getcwd(), 'credentials.json')
-    credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=SCOPES)
-    client = gspread.authorize(credentials)
-    gc = gspread.service_account(filename=secret_file)
-    #opening the created spreadsheet
-    worksheet = gc.open_by_key(id).get_worksheet(0)
-    with open ('bucketOfPickles', 'rb') as bop:
-        buckets = pickle.load(bop)
-    #print(buckets)
-    df=np.array(buckets)
-    worksheet.update(df.tolist())
-    print(worksheet)
+import numpy as np #need
 
 def create_spreadsheet():
-    #Scopes that we are using for the APIs
+    #Scopes that we are using for the APIs #TODO: maybe make auth a function?
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
           'https://www.googleapis.com/auth/drive']
           #Credentials
@@ -55,10 +35,32 @@ def create_spreadsheet():
         'parents': [folder_id]
         }
     file = service.files().create(body=file_metadata).execute()
-    global id
+    global id #TODO: make sure this is the best way to set the id variable for use in other function
     id = file['id']
-    #print(file)
-    print(id)
+    #print(file) # TODO: delete. using for testing
+    print(id) # TODO: can comment out/del this line. using it for testing to track id value
+
+def populate_spreadsheet():
+    #passing in created spreadsheet id from create_spreadsheet function
+    #TODO: maybe make auth a function?
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
+                  'https://www.googleapis.com/auth/drive']
+    #Credentials
+    secret_file = os.path.join(os.getcwd(), 'credentials.json')
+    credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=SCOPES)
+    client = gspread.authorize(credentials)
+    gc = gspread.service_account(filename=secret_file)
+    #opening the created spreadsheet based on id num
+    worksheet = gc.open_by_key(id).get_worksheet(0)
+    # TODO: Delete use of test data
+    with open ('bucketOfPickles', 'rb') as bop:
+        buckets = pickle.load(bop)
+    #print(buckets)
+    # END OF TEST DATA
+    # TODO: set up ability to read in the dictionary
+
+    print(worksheet)
+
 
 def main():
     # Running my create spreadsheet function
